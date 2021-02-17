46b4a520dcd5d3b937a6e5b9a15042ecc5357f6e
liulanz <zhengliulan@gmail.com>
2021-02-17 12:31:53 -0500

    finish basic requirements for part 2

---
+++ app/src/main/java/com/example/flixster2/adapters/MovieAdapter.java
@@ -0,0 +1,103 @@
+package com.example.flixster2.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster2.DetailActivity;
+import com.example.flixster2.R;
+import com.example.flixster2.models.Movie;
+
+import com.example.flixster2.R;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    Context context;
+    List<Movie> movies;
+    public MovieAdapter(Context context, List<Movie>movies){
+        this.context= context;
+        this.movies = movies;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        Log.d("MovieAdapter: ","onCreateHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent,false );
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        Log.d("MovieAdapter: ","onBindViewHolder " +position);
+        Movie movie = movies.get(position);
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvTitle= itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+            //landscape
+            if (context.getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            }
+            else{
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+
+            // 1.Register click listener on the whole row
+            // 2. Navigate to a new activity on tap
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+//                    toast is a way to debug
+//                    Toast.makeText(context, movie.getTitle() ,Toast.LENGTH_SHORT).show();
+                    // open a new activity
+                    Intent i = new Intent (context, DetailActivity.class);
+                    // android doesn't recognize movie object by default
+                    // we need to make movie object to parcelable through a library
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+        }
+    }
+}
